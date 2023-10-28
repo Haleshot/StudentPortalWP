1516f857d77a92d3c16201e4b08e05fadb2663df
Github <tilwaniavneesh1234@gmail.com>
2023-10-28 22:26:48 +0530

    This consists of three new files which are added to code that are settings.html , settings.js, settings.css in the respective folders. this part consists of updation part of our project. it allows user to update his credentials.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,6 +3,7 @@ const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('close-btn');
 
 const darkMode = document.querySelector('.dark-mode');
+ 
 
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
@@ -17,6 +18,52 @@ darkMode.addEventListener('click', () => {
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
+
+import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+
+const db = getDatabase();
+const usersRef = ref(db, "users");
+// const sapid = localStorage.getItem("sapid");
+
+
+
+// get(child(usersRef, sapid)).then((snapshot) => {
+// if (snapshot.exists()) {
+//     if (snapshot.child('name').exists()) {
+                
+//         var studentName = snapshot.val().name;
+//         document.getElementById('username').innerHTML = 'Welcome, ' + studentName;
+//     } else {
+//         document.getElementById('username').innerHTML = 'Welcome, Student';
+//     }
+//     } else {
+//     alert("user does not exists!!");
+//     }
+// });   
+
+//   var user = firebaseConfig.auth().currentUser;
+// if (user) {
+//     // getting reference to students data in database
+//     var studentRef = firebaseConfig.database().ref('students/' + sapid);
+
+//     // takinng students data
+//     usersRef.once('value').then(function(snapshot) {
+//         // To check user data is there or not
+//         if (snapshot.child('name').exists()) {
+//             var studentName = snapshot.val().name;
+//             document.getElementById('username').innerHTML = 'Welcome, ' + studentName;
+//         } else {
+//             document.getElementById('username').innerHTML = 'Welcome, Student';
+//         }
+//     }).catch(function(error) {
+//         console.error("Error fetching student data: ", error);
+//     });
+// } else {
+//     // when user has not logged in
+//     document.getElementById('userName').innerHTML = 'Welcome, Guest';
+// }
+
 
 // Passing Login info from login.js to dashboard.js
 
--- public/js/login.js
+++ public/js/login.js
@@ -7,15 +7,23 @@ document.getElementById("register").addEventListener("click", function(e) {
   const sapid = document.getElementById("sapid").value;
   const password = document.getElementById("password").value;
 
+  localStorage.setItem("sapid",id);
+
   // Create a reference to the "users" collection in Firebase
   const db = getDatabase();
   const usersRef = ref(db, "users");
+  
 
   // Check if the SAP ID already exists in Firebase
   get(child(usersRef, sapid)).then((snapshot) => {
     if (snapshot.exists()) {
       if (snapshot.val().password == password) {
         alert("Login Successful");
+        var inputValue = document.getElementById('sapid').value;
+
+        // Call a function in another script file and pass the value
+        // handleFormSubmission(inputValue);
+        // console.log(inputValue);
         window.location.href = "dashboard.html";
       } else {
         alert("Incorrect Password");
@@ -26,6 +34,40 @@ document.getElementById("register").addEventListener("click", function(e) {
     }
   });
 });
+
+function handleFormSubmission(sapid, password) {
+  // You can use 'sapid' and 'password' in script2.js
+  console.log("SAP ID:", sapid);
+  console.log("Password:", password);
+  // ... rest of the code in script2.js
+  window.location.href = "dashboard.html";
+}
+// function submitForm(){
+//   var inputValue = document.getElementById('sapid').value;
+//   handleFormSubmission(inputValue);
+// }
+
+
+// if (user) {
+//     // Get a reference to the student's data in the database
+//     var studentRef = firebase.database().ref('students/' + user.uid);
+
+//     // Fetch the student's data
+//     studentRef.once('value').then(function(snapshot) {
+//         // Check if the student has a name stored in the database
+//         if (snapshot.child('name').exists()) {
+//             var studentName = snapshot.val().name;
+//             document.getElementById('userName').innerHTML = 'Welcome, ' + studentName;
+//         } else {
+//             document.getElementById('userName').innerHTML = 'Welcome, Student';
+//         }
+//     }).catch(function(error) {
+//         console.error("Error fetching student data: ", error);
+//     });
+// } else {
+//     // Handle the case where the user is not logged in
+//     document.getElementById('userName').innerHTML = 'Welcome, Guest';
+// }
 
 // public/js/login.js
 // document.getElementById("register").addEventListener("click", function(e) {
